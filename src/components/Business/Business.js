cd47ffc736bb650892be74c4e05795d8f1d14914
hannahkim1 <xhannahkim1@gmail.com>
2022-10-25 10:08:41 -0700

    finished the restaurant section

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -12,12 +12,18 @@ class Business extends React.Component {
                     <h2>{this.props.business.name}</h2>
                     <div className="Business-information">
                         <div className="Business-address">
+                            <div className="Business-titles">
+                                <p>Address: </p>
+                            </div>
                             <p>{this.props.business.address}</p>
                             <p>{this.props.business.city}</p>
                             <p>{this.props.business.state} {this.props.business.zipCode}</p>
                         </div>
                         <div className="Business-reviews">
-                            <h3>{this.props.business.category}</h3>
+                            <div className="Business-titles">
+                                <p>Information: </p>
+                            </div>
+                            <p>{this.props.business.category}</p>
                             <h3 className="rating">{this.props.business.rating} stars</h3>
                                 <p>{this.props.business.reviewCount} reviews</p>
                                 <p>{this.props.business.price}</p>

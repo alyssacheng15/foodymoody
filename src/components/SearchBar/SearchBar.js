42a8624228f5f34b8aeacb98790eeaede522b7d0
alyssacheng15 <105820811+alyssacheng15@users.noreply.github.com>
2022-10-24 21:03:07 -0700

    updated mood buttons

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,39 +14,63 @@ class SearchBar extends React.Component {
             term: '',
             location: '',
             sortBy: 'best_match',
-            category1: 'cake',
-            category2: 'tacos',
-            category3: 'ramen',
-            category4: 'burgers'
+            categoryHappy: 'smoothie',
+            categorySad: 'cake',
+            categoryStressed: 'tea',
+            categoryAngry: 'salad',
+            categoryCold: 'soup',
+            categoryCovid: 'fruit',
+            categoryHungover: 'breakfast',
+            categoryPregnant: 'burger',
         };
 
         this.handleTermChange = this.handleTermChange.bind(this);
         this.handleLocationChange = this.handleLocationChange.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
-        this.happy = this.happy.bind(this);
-        this.sad = this.sad.bind(this);
-        this.stressed = this.stressed.bind(this);
-        this.angry = this.angry.bind(this);
+        this.Happy = this.Happy.bind(this);
+        this.Sad = this.Sad.bind(this);
+        this.Stressed = this.Stressed.bind(this);
+        this.Angry = this.Angry.bind(this);
+        this.Cold = this.Cold.bind(this);
+        this.Covid = this.Covid.bind(this);
+        this.Hungover = this.Hungover.bind(this);
+        this.Pregnant = this.Pregnant.bind(this);
     }
 
-    happy(event) {
-        this.props.searchYelp(this.state.category1);
+    Happy(event) {
+        this.props.searchYelp(this.state.categoryHappy);
     }
 
-    sad(event) {
-        this.props.searchYelp(this.state.category2);
+    Sad(event) {
+        this.props.searchYelp(this.state.categorySad);
     }
 
-    stressed(event) {
-        this.props.searchYelp(this.state.category3);
+    Stressed(event) {
+        this.props.searchYelp(this.state.categoryStressed);
     }
 
-    angry(event) {
-        this.props.searchYelp(this.state.category4);
+    Angry(event) {
+        this.props.searchYelp(this.state.categoryAngry);
+    }
+
+    Cold(event) {
+        this.props.searchYelp(this.state.categoryCold);
+    }
+
+    Covid(event) {
+        this.props.searchYelp(this.state.categoryCovid);
+    }
+
+    Hungover(event) {
+        this.props.searchYelp(this.state.categoryHungover);
+    }
+
+    Pregnant(event) {
+        this.props.searchYelp(this.state.categoryPregnant);
     }
     
     handleSearch(event) {
-        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        this.props.searchYelp(this.state.term, this.state.sortBy);
         event.preventDefault();
     }
 
@@ -84,23 +108,17 @@ class SearchBar extends React.Component {
                 </div>
                 
                 <div className="moodButtons">
-                        <button onClick={this.happy}>happy</button>
-                        <button onClick={this.sad}>sad</button>
-                        <button onClick={this.stressed}>stressed</button>
-                        <button onClick={this.angry}>angry</button>
+                        <button onClick={this.Happy}>Happy</button>
+                        <button onClick={this.Sad}>Sad</button>
+                        <button onClick={this.Stressed}>Stressed</button>
+                        <button onClick={this.Angry}>Angry</button>
+                        <button onClick={this.Cold}>Cold</button>
+                        <button onClick={this.Covid}>Covid</button>
+                        <button onClick={this.Hungover}>Hungover</button>
+                        <button onClick={this.Pregnant}>Pregnant</button>
                 </div>
 
                 <div className="SearchBar">
-                    <div className="test">
-                        <ul>
-                            <li>this is just a test:</li>
-                            <li>happy = cake</li>
-                            <li>sad = tacos</li>
-                            <li>stressed = ramen</li>
-                            <li>angry = burgers</li>
-                        </ul>
-                    </div>
-                
                     <div className="SearchBar-fields">
                         <input placeholder="Search Food" onChange={this.handleTermChange} />
                     </div>
